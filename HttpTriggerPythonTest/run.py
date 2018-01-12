c2b45ed45f80bd7ac95d193e115cdf89586e7e70
smcgee30 <smcgee30@gmail.com>
2018-01-12 10:23:09 -0500

    ugh

---
+++ HttpTriggerPythonTest/run.py
@@ -0,0 +1,16 @@
+import sys, os, calendar, json
+from datetime import datetime, timedelta
+import os
+import argparse
+
+postreqdata = json.loads(open(os.environ['req']).read())
+response = open(os.environ['res'], 'w')
+data = {
+   'name' : postreqdata['name'],
+   'dash' : postreqdata['dash'],
+   'price' : 542.23
+}
+
+json_str = json.dumps(data)
+response.write(json_str)
+response.close()
